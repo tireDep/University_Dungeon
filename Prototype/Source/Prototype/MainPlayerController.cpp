b75683fcf3335543d0e5030b8ff0c9e4d982bf15
_tireDep <wing997@naver.com>
2024-10-14 00:02:39 +0900

    프로토타입 프로젝트 - UI 테스트

---
+++ Prototype/Source/Prototype/MainPlayerController.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainPlayerController.h"
+#include "Blueprint/UserWidget.h"
+
+void AMainPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* MainScreen = CreateWidget(this, MainScreenClass);
+	if (MainScreen == nullptr)
+	{
+		// error
+		return;
+	}
+
+	MainScreen->AddToViewport();
+}
---
+++ Prototype/Source/Prototype/MainPlayerController.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MainPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTOTYPE_API AMainPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UUserWidget> MainScreenClass;
+};
